ee753e4b642c70a4ebc700887f58505d5ec6b1a3
gafeol <gabfeol@gmail.com>
2018-09-10 13:20:04 -0300

    Curso 3 - Aula 1

---
+++ C3/Aula 1/fogefoge.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include "fogefoge.hpp"
+using namespace std;
+
+char** mapa;
+int linhas;
+int colunas;
+
+void le_mapa() {
+	ifstream mapa_input;
+
+	mapa_input.open(ARQUIVO_MAPA);
+	if(mapa_input.is_open()) {
+
+		mapa_input >> linhas >> colunas;
+		aloca_mapa();
+
+		for (int i = 0; i < linhas; i++) {
+			for(int j = 0; j < colunas; j++){
+				mapa_input >> mapa[i][j];
+			}
+		}
+
+		mapa_input.close();
+	}
+	else {
+		cout << "Erro na leitura do mapa" << endl;
+		exit(1);
+	}
+}
+
+void aloca_mapa() {
+	mapa = (char**) malloc(sizeof(char*) * linhas);
+
+	for(int i = 0; i < linhas; i++) {
+		mapa[i] = (char*) malloc(sizeof(char) * colunas + 1);
+	}
+}
+
+void libera_mapa() {
+	for(int i = 0; i < linhas; i++) {
+		free(mapa[i]);
+	}
+	free(mapa);
+}
+
+void imprime_mapa() {
+	for(int i=0;i<linhas;i++){
+		for(int j=0;j<colunas;j++){
+			cout << mapa[i][j];
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	
+	le_mapa();
+
+	imprime_mapa();
+
+	libera_mapa();
+}
---
+++ C3/Aula 1/fogefoge.hpp
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+const string ARQUIVO_MAPA = "mapa.txt";
+
+void aloca_mapa();
+void le_mapa();
+void libera_mapa();
+void imprime_mapa();
