ed6f94b726d65cc4d4f020a1062651dbf9118f64
gafeol <gabfeol@gmail.com>
2018-09-24 14:01:36 -0300

    Adiciona partes de aula C2 - Aula 2

---
+++ C2/Aula2/2/forca.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main() {
+    const string PALAVRA_SECRETA = "MELANCIA";
+    cout << PALAVRA_SECRETA << endl;
+}
---
+++ C2/Aula2/3/forca.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main() {
+    const string PALAVRA_SECRETA = "MELANCIA";
+    cout << PALAVRA_SECRETA << endl;
+
+    bool nao_ganhou, nao_enforcou;
+
+    do{
+
+    } while(nao_ganhou && nao_enforcou);
+}
